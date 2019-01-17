4a48579a7a54459363267cb48f792df320ef1e04
Mike Young <michael_y@live.co.uk>
2019-01-17 15:26:06 +0000

    add key for party
    adding key for temp data for party to clear console error stating no keys

--- src/components/Party.js
+++ src/components/Party.js
@@ -3,12 +3,12 @@ import React, { Component } from "react";
 class Party extends Component {
   state = {
     pokemon: [
-      { name: "pika", content: "eletric type pokemon" },
-      { name: "charmander", content: "fire type pokemon" },
-      { name: "charmander", content: "fire type pokemon" },
-      { name: "charmander", content: "fire type pokemon" },
-      { name: "charmander", content: "fire type pokemon" },
-      { name: "charmander", content: "fire type pokemon" }
+      { id: 1, name: "pika", content: "eletric type pokemon" },
+      { id: 2, name: "charmander", content: "fire type pokemon" },
+      { id: 3, name: "charmander", content: "fire type pokemon" },
+      { id: 4, name: "charmander", content: "fire type pokemon" },
+      { id: 5, name: "charmander", content: "fire type pokemon" },
+      { id: 6, name: "charmander", content: "fire type pokemon" }
     ]
   };
 
@@ -17,7 +17,7 @@ class Party extends Component {
 
     const partyList = this.state.pokemon.map(pokemon => {
       return (
-        <div className="card col-sm">
+        <div className="card col-sm" key={pokemon.id}>
           <img className="card-img-top" src={img} alt="Card cap" />
           <div className="card-body">
             <h5 className="card-title">{pokemon.name}</h5>
